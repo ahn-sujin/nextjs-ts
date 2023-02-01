6b1ddc3cc24a50ab3ecb376cec93ff212ba44d22
ahn-sujin <asj9674@gmail.com>
2023-02-01 23:18:53 +0900

    Add:header 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
-const Home = () => {
-  return <p>ASJ의 성공적인 이직기 스따또!</p>;
-};
+import { Fragment } from 'react';
+import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
+import Link from 'next/link';
+import { VscFeedback } from 'react-icons/vsc';
+import { AiOutlineShareAlt } from 'react-icons/ai';
 
-export default Home;
+export default function Home() {
+  return (
+    <Fragment>
+      <Header
+        rightElements={[
+          <button
+            onClick={() => {
+              alert('복사!');
+            }}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link href="/feedback" className={styles.box} key="link">
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      />
+      <main></main>
+    </Fragment>
+  );
+}
