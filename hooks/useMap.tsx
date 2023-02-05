da940a6b473c413fdecd23eaec9979e781eaa073
ahn-sujin <asj9674@gmail.com>
2023-02-05 20:27:00 +0900

    Add: useMap custom hook 작업

--- hooks/useMap.tsx
+++ hooks/useMap.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react';
-import { mutate } from 'swr';
+import useSWR, { mutate } from 'swr';
 import type { Coordinates } from '../types/store';
 import type { NaverMap } from '../types/map';
 
@@ -9,12 +9,31 @@ export const INITIAL_ZOOM = 10;
 export const MAP_KEY = '/map';
 
 const useMap = () => {
+  const { data: map } = useSWR(MAP_KEY);
+
   const initializeMap = useCallback((map: NaverMap) => {
     mutate(MAP_KEY, map);
   }, []);
 
+  // INITIAL_CENTER와 INITIAL_ZOOM으로 맵의 좌표와 줌 레벨을 변경한다.
+  const resetMapOprtions = useCallback(() => {
+    /** https://navermaps.github.io/maps.js.ncp/docs/naver.maps.Map.html#morph__anchor */
+    map.morph(new naver.maps.LatLng(...INITIAL_CENTER), INITIAL_ZOOM);
+  }, [map]);
+
+  // 현재 지도의 중심 좌표의 지도 레벌을 리턴한다.
+  const getMapOptions = useCallback(() => {
+    const mapCenter = map.getCenter();
+    const center: Coordinates = [mapCenter.lat(), mapCenter.lng()];
+    const zoom = map.getZoom();
+
+    return { center, zoom };
+  }, [map]);
+
   return {
     initializeMap,
+    resetMapOprtions,
+    getMapOptions,
   };
 };
 
