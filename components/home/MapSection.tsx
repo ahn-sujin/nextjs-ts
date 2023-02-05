5b420b0316adefc330065c8b36be3dd5d26469ce
ahn-sujin <asj9674@gmail.com>
2023-02-05 20:54:42 +0900

    Add:url에 있는 위경도 값으로 초기화면 세팅

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -1,10 +1,33 @@
+import { useMemo } from 'react';
+import { useRouter } from 'next/router';
 import Map from './Map';
 import Markers from './Markers';
-import useMap from '../../hooks/useMap';
+import useMap, { INITIAL_CENTER, INITIAL_ZOOM } from '../../hooks/useMap';
 import useCurrentStore from '../../hooks/useCurrentStore';
 import type { NaverMap } from '../../types/map';
+import { Coordinates } from '../../types/store';
 
 const MapSection = () => {
+  /** url query로 부터 initial zoom, center 값 설정 */
+  const router = useRouter();
+  /**
+   * router.asPath === `/?zoom{}&lat={}&lng={}`
+   * https://developer.mozilla.org/ko/docs/Web/API/URLSearchParams
+   */
+  const query = useMemo(() => new URLSearchParams(router.asPath.slice(1)), []);
+  const initialZoom = useMemo(
+    () => (query.get('zoom') ? Number(query.get('zoom')) : INITIAL_ZOOM),
+    [query]
+  );
+  const initialCenter = useMemo<Coordinates>(
+    () =>
+      query.get('lat') && query.get('lng')
+        ? [Number(query.get('lat')), Number(query.get('lng'))]
+        : INITIAL_CENTER,
+    [query]
+  );
+
+  /** onLoadMap */
   const { initializeMap } = useMap();
   const { clearCurrentStore } = useCurrentStore();
 
@@ -15,7 +38,11 @@ const MapSection = () => {
 
   return (
     <>
-      <Map onLoad={onLoadMap} />
+      <Map
+        onLoad={onLoadMap}
+        initialZoom={initialZoom}
+        initialCenter={initialCenter}
+      />
       <Markers />
     </>
   );
