4e910e19e32ef60a9f1808dc93b3812e1fa32de7
ahn-sujin <asj9674@gmail.com>
2023-02-09 23:54:30 +0900

    Add:google analytics 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import Script from 'next/script';
 import SEO from '../seo.config';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8ZFD6DRJE7');
+            `,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8ZFD6DRJE7`}
+      />
       <Component {...pageProps} />
     </>
   );
