387c4b87b1f0d6f43631524967d78bcc3ee73cd1
ahn-sujin <asj9674@gmail.com>
2023-02-23 23:16:29 +0900

    Modify: detail page style component 적용

--- pages/detail/[name].tsx
+++ pages/detail/[name].tsx
@@ -6,6 +6,7 @@ import type { Store } from 'types/store';
 import DetailHeader from 'views/Home/Detail/DetailHeader';
 import DetailContent from 'views/Home/Detail/DetailContent';
 import styles from 'styles/detail.module.scss';
+import styled from 'styled-components';
 
 interface Props {
   store: Store;
@@ -34,16 +35,16 @@ const StoreDetail: NextPage<Props> = ({ store }) => {
           url: `https://sujin-map.vercel.app/detail/${store.name}`,
         }}
       />
-      <div
-        className={`${styles.detailSection} ${styles.expanded} ${styles.selected} `}
-      >
-        <DetailHeader
-          currentStore={store}
-          expanded={expanded}
-          onClickArrow={goToMap}
-        />
-        <DetailContent currentStore={store} expanded={expanded} />
-      </div>
+      <Styled>
+        <div className="detail_section selected expanded">
+          <DetailHeader
+            currentStore={store}
+            expanded={expanded}
+            onClickArrow={goToMap}
+          />
+          <DetailContent currentStore={store} expanded={expanded} />
+        </div>
+      </Styled>
     </>
   );
 };
@@ -64,3 +65,34 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   return { props: { store } };
 };
+
+const Styled = styled.div`
+  height: 100%;
+  > .detail_section {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%) !important;
+    width: 430px;
+    height: 100%;
+    z-index: 101;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 0 0;
+    background-color: white;
+    color: #444444;
+    border-top-left-radius: 24px;
+    border-top-right-radius: 24px;
+    box-shadow: 0 -2px 8px 0 rgba(136, 136, 136, 0.3);
+    transition: transform 800ms;
+    transform: translateY(calc(100% - 68px));
+
+    &.selected {
+      transform: translateY(calc(100% - 160px));
+    }
+
+    &.expanded {
+      transform: translateY(0);
+    }
+  }
+`;
