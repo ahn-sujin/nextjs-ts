c1812ad6d914cfafa935aa9a5efdc562e4fabc48
ahn-sujin <asj9674@gmail.com>
2023-03-12 23:07:03 +0900

    Modify:초기 진입 시 CSS in JS가 적용되지 않는 문제 해결

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,69 @@
-import Documnet, { Html, Head, Main, NextScript } from 'next/document';
-import Link from 'next/link';
+// import Documnet, { Html, Head, Main, NextScript } from 'next/document';
+// import Link from 'next/link';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <Link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// export default function Document() {
+//   return (
+//     <Html>
+//       <Head>
+//         <Link
+//           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+//           rel="stylesheet"
+//         />
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet();
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => App,
+        enhanceComponent: (Component) => Component,
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
